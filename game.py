7b0ad40f6d174e9c0564e50509c3668cf4fe2850
NoNeedForQuestions <105724486+AlexanderFrom100@users.noreply.github.com>
2023-05-07 16:14:35 +0300

    Update game.py

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ import pygame
 
 w = 10
 h = 20
-Tile = 45
+Tile = 35
 screen = w * Tile, h * Tile
 fps = 60
 
@@ -12,12 +12,22 @@ clock = pygame.time.Clock()
 
 grid = [pygame.Rect(x * Tile, y * Tile, Tile, Tile) for x in range(w) for y in range(h)]
 
+figure_pos = [[(-1,0),(-2,0),(0,0),(1,0)],
+              [(0,-1),(-1,-1),(-1,0),(0,0)],
+              [(-1,0),(-1,1),(0,0),(0,-1)],
+              [(0,0),(-2,0),(0,0),(1,0)],
+              [(0,0),(-2,0),(0,0),(1,0)],
+              [(0,0),(-2,0),(0,0),(1,0)],
+              [(0,0),(-2,0),(0,0),(1,0)]]
+
 while True:
     game_screen.fill(pygame.Color(200,200,200))
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
+    
+    [pygame.draw.rect(game_screen, (100,100,100), i_rect, 1) for i_rect in grid]
             
     pygame.display.flip()
     clock.tick(fps)
