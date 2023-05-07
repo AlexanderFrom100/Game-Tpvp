212ed86611dc2b84c1c2c87376ca80505c4c5250
NoNeedForQuestions <105724486+AlexanderFrom100@users.noreply.github.com>
2023-05-07 15:29:46 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,10 +1,16 @@
 import pygame 
 
-screen = 800, 600
+w = 10
+h = 20
+Tile = 45
+screen = w * Tile, h * Tile
+fps = 60
 
 pygame.init()
 game_screen = pygame.display.set_mode(screen)
 clock = pygame.time.Clock()
+
+grid = [pygame.Rect(x * Tile, y * Tile, Tile, Tile) for x in range(w) for y in range(h)]
 
 while True:
     game_screen.fill(pygame.Color(200,200,200))
@@ -14,4 +20,4 @@ while True:
             exit()
             
     pygame.display.flip()
-    clock.tick()
+    clock.tick(fps)
