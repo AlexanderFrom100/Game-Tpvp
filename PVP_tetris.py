2a69083b0f0880f79eb46bef17420c51683495bf
NoNeedForQuestions <105724486+AlexanderFrom100@users.noreply.github.com>
2023-06-05 00:20:28 +0300

    Update PVP_tetris.py

--- PVP_tetris.py
+++ PVP_tetris.py
@@ -575,15 +575,15 @@ def twodshoot():
             dx = 0
             dy = 0
             
-            if self.jump == True and self.in_air == False:
-                self.vel_y = -11
-                self.jump = False
-                self.in_air = True
-
             self.vel_y += GRAVITY
             if self.vel_y > 10:
                 self.vel_y
             dy += self.vel_y
+
+            if self.jump == True and self.in_air == False:
+                self.vel_y = -11
+                self.jump = False
+                self.in_air = True
 
             if moving_left:
                 dx = -self.speed
